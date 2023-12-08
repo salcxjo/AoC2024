b79a7dd7cb0a140f8c035cb26050cc7e5ebd49ce
Mario Liebisch <mario.liebisch@gmail.com>
2023-12-08 08:53:08 +0100

    Day 7

--- cmd/template/main.go
+++ cmd/template/main.go
@@ -28,6 +28,6 @@ func main() {
 	print("Example 1: ", solve1(Data+"/example.txt"), "\n")
 	print("Solution 1: ", solve1(Data+"/input.txt"), "\n")
 
-	print("Example 1: ", solve2(Data+"/example.txt"), "\n")
+	print("Example 2: ", solve2(Data+"/example.txt"), "\n")
 	print("Solution 2: ", solve2(Data+"/input.txt"), "\n")
 }
